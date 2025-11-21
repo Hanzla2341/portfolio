ce1a07988431124475453d1f084b760109e84f55
Hanzla2341 <hanzlasabir006@gmail.com>
2025-11-20 20:31:47 -0800

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import './App.css'
 import Services from './components/Services'
 
 import Contact from './components/Contact'
-import Profile from './components/profile'
+
 import {Routes,Route} from 'react-router-dom'
 import Portfolio from './components/portfolio'
+import Profile from './components/profile'
 
 const App = () => {
 
